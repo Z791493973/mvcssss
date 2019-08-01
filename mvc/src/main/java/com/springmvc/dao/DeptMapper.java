eb15f075ef31b36ceaa4cfebffc702d5430ae825
张指导 <@DESKTOP-UUJJLOC>
2019-08-01 21:45:26 +0800

    ASDF

--- mvc/src/main/java/com/springmvc/dao/DeptMapper.java
+++ mvc/src/main/java/com/springmvc/dao/DeptMapper.java
@@ -12,21 +12,21 @@ import java.util.List;
 @Repository("deptMapper")
 public interface DeptMapper {
 
-    //新增
+    //鏂板
     @Insert("insert into dept(deptno,dname,loc)values(#{deptno},#{dname},#{loc})")
     int add(Dept dept);
-    //修改
+    //淇敼
     @Update("update dept set dname=#{dname},loc=#{loc} where deptno=#{deptno}")
     int update(Dept dept);
 
-    //删除
+    //鍒犻櫎
     @Delete("delete from dept where deptno=#{deptno}")
     int delete(Integer deptno);
 
-    //查询所有
+    //鏌ヨ鎵�鏈�
     @Select("select deptno,dname,loc from dept")
     List<Dept> find();
-    //根据id查询
+    //鏍规嵁id鏌ヨ
     @Select("select deptno,dname,loc from dept where deptno=#{deptno}")
     Dept findById(Integer deptno);
 
